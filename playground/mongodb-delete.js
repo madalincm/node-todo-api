e348600969d5357e6e8743053d95199a74bfa308
madalincm <madalin.cotetiu@softvision.ro>
2018-05-10 18:27:54 +0300

    delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+//const MongoClient = require("mongodb").MongoClient;
+const {MongoClient, ObjectID} = require("mongodb");
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) =>{
+    if(err){
+        return console.log("Unable to connect to MongoDb");
+    }
+    console.log("Connected to MongoDB server");
+    const db = client.db("TodoApp")
+
+
+    //deletaMany
+    // db.collection("Todos").deleteMany({text:"eat lunch"}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    //deleteOne
+    // db.collection("Todos").deleteOne({text:"Walk the dog"}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    //FindOneAndDelete
+    db.collection("Todos").findOneAndDelete({completed: true}).then((result) => {
+        console.log(result);
+    });
+
+    //client.close();
+});
