5c2faca345d8c2d6665013f7f7225e82bdb672be
madalincm <madalin.cotetiu@softvision.ro>
2018-05-18 18:07:43 +0300

    test

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post("/todos", (req, res) =>{
+app.post("/todos", authenticate, (req, res) =>{
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
     todo.save().then((doc) =>{
         res.send(doc);
@@ -26,21 +27,26 @@ app.post("/todos", (req, res) =>{
     });
 });
 
-app.get("/todos", (req,res) => {
-    Todo.find().then((todos) => {
+app.get("/todos", authenticate, (req,res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
 });
 
-app.get("/todos/:id", (req,res) => {
+app.get("/todos/:id", authenticate, (req,res) => {
     var id = req.params.id;
     if(!ObjectId.isValid(id)){
         return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) =>{
+    Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) =>{
         if(!todo){
             return res.status(404).send();
         }
@@ -50,13 +56,16 @@ app.get("/todos/:id", (req,res) => {
     });
 });
 
-app.delete("/todos/:id", (req,res) => {
+app.delete("/todos/:id", authenticate, (req,res) => {
     var id = req.params.id;
     if(!ObjectId.isValid(id)){
         return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if(!todo){
             return res.status(404).send();
         }
@@ -67,7 +76,7 @@ app.delete("/todos/:id", (req,res) => {
 
 });
 
-app.patch("/todos/:id", (req,res) => {
+app.patch("/todos/:id", authenticate, (req,res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ["text", "completed"]);
 
@@ -81,8 +90,11 @@ app.patch("/todos/:id", (req,res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(
-        id, 
+    Todo.findOneAndUpdate(
+        {
+            _id: id,
+            _creator: req.user._id
+        }, 
         {$set: body}, 
         {new: true}).then((todo) => {
             if (!todo) {
@@ -118,12 +130,22 @@ app.post('/users', (req, res) => {
 app.post("/users/login", (req,res) => {
     var body = _.pick(req.body, ['email', 'password']);
     User.findByCredentials(body.email, body.password).then((user) => {
-        res.send(user);
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
     }).catch((e) => {
         res.status(400).send();
     });
 });
+//DELETE /users/login {email, password}
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+      res.status(200).send();
+    }, () => {
+      res.status(400).send();
+    });
+  });
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
